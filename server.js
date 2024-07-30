ee9fcfe40f3f2fba60b8a380e9eb865fd997fe75
Anshu Chauhan <109979816+anshuchauhan13@users.noreply.github.com>
2024-07-30 18:53:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 connectDB();
 app.use(
   cors({
-    origin: "http://localhost:3000/api/users",
+    origin: "https://user-info-134g.onrender.com",
   })
 );
 app.use(bodyParser.json());
